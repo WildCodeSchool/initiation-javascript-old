9e1b865fafc8d50b71b6adaeed0896cc839a1fbf
Benoît Hubert <benoithubert@gmail.com>
2019-09-13 13:52:49 +0200

    First build

---
+++ precache-manifest.691c34accbc50efd9ffc9bf88d2ad47a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca72bc129c3998f3edc2df25478830b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e26488c6c9908ffa58",
+    "url": "/static/js/2.a9fe5b44.chunk.js"
+  },
+  {
+    "revision": "8f296bc0779080f2ab6c",
+    "url": "/static/js/main.b682848d.chunk.js"
+  },
+  {
+    "revision": "fcec1b7fb2254c15c913",
+    "url": "/static/js/runtime~main.829794dd.js"
+  }
+]);
